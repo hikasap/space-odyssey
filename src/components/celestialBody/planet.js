f96f98b7ba0a2c468ceb10afa979a0e2c52f48f3
halil <halilibrahimkasapoglu@gmail.com>
2025-01-01 13:35:48 +0300

    feat: update orbital celestials

--- src/components/celestialBody/planet.js
+++ src/components/celestialBody/planet.js
@@ -1,12 +1,25 @@
 import { CelestialBody } from './celestialBody.js';
 
 export class Planet extends CelestialBody {
-    constructor(size, color) {
+    constructor(size, color, semiMajorAxis, eccentricity, orbitalPeriod) {
         super(size, color, 'sphere');
-        this.orbitSpeed = Math.random() * 0.01;
+        this.semiMajorAxis = semiMajorAxis;
+        this.eccentricity = eccentricity;
+        this.orbitalPeriod = orbitalPeriod;
+        this.orbitalAngle = Math.random() * Math.PI * 2; // Random initial angle
     }
 
-    orbit() {
-        // Implement orbit logic if needed
+    updateOrbit(deltaTime) {
+        const angleIncrement = (2 * Math.PI / this.orbitalPeriod) * deltaTime;
+        this.orbitalAngle += angleIncrement;
+
+        const a = this.semiMajorAxis;
+        const e = this.eccentricity;
+        const b = a * Math.sqrt(1 - e * e);
+
+        const x = a * Math.cos(this.orbitalAngle);
+        const y = b * Math.sin(this.orbitalAngle);
+
+        this.mesh.position.set(x, 0, y);
     }
 }
