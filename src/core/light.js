41d0cf9fdf00b48a8af42482fe99a058aadefc3d
halil <halilibrahimkasapoglu@gmail.com>
2025-01-01 16:07:57 +0300

    fix: reduce ambient color

--- src/core/light.js
+++ src/core/light.js
@@ -3,5 +3,5 @@ import * as THREE from 'three';
 const light = new THREE.PointLight(0xffffff, 1, 100);
 light.position.set(0, 0, 0);
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
 export { light, ambientLight };
