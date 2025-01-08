6d5749985fce861f31fc4ccf8e6805868ca1cea2
halil <halilibrahimkasapoglu@gmail.com>
2025-01-08 16:46:01 +0300

    feat: add motion blurred star effect

--- src/scenes/scene.js
+++ src/scenes/scene.js
@@ -1,4 +1,4 @@
-import scene from '../core/scene.js';
+// import scene from '../core/scene.js';
 import Camera from '../core/camera.js';
 import renderer from '../core/renderer.js';
 import { ambientLight } from '../core/light.js';
@@ -9,21 +9,37 @@ import { Moon } from '../components/celestialBody/moon.js';
 import * as THREE from 'three';
 import { Spacecraft } from '../components/spacecraft.js';
 import PhysicsInstance from '../core/physics.js';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
+import { AfterimagePass } from 'three/examples/jsm/postprocessing/AfterimagePass.js';
 
+let speedMultiplier = 1.0;
 
-export function initScene(container){
+export function setSpeedMultiplier(value) {
+    speedMultiplier = value;
+}
 
+let scene = null;
+
+export function displayScene(container){
+    if (!scene) {
+        initScene();
+    }
+    container.appendChild(renderer.domElement);
+}
+
+export function initScene(){
+
+    scene = new THREE.Scene();
     renderer.setSize(window.innerWidth, window.innerHeight);
-    
-    const camera = new Camera(75, window.innerWidth / window.innerHeight, 0.01, 1000);
-    camera.position.set(0, 0, 100);
+
+    const camera = new Camera(75, window.innerWidth / window.innerHeight, 0.01, 2048);
     camera.setOrbitControls(renderer.domElement);
 
     scene.add(ambientLight);
-    container.appendChild(renderer.domElement);
 
     const celestialBodies = [];
-    const chunkSize = 256;
+    const chunkSize = 512;
     const x_range = [0];
     const y_range = [0];
     const z_range = [0];
@@ -31,16 +47,55 @@ export function initScene(container){
         for (const y of y_range) {
             for (const z of z_range) {
                 const chunkOffset = new THREE.Vector3(x * chunkSize, y * chunkSize, z * chunkSize);
-                generateSolarSystem(scene, celestialBodies, chunkOffset);
+                generateSolarSystem(scene, celestialBodies, chunkOffset, chunkSize);
             }
         }
     }
+
+
+    function getSphericalRandomDot(radius)
+    {
+        const u = Math.random();
+        const v = Math.random();
+    
+        const phi = 2 * Math.PI * u;
+        const theta = Math.acos(2 * v - 1);
+    
+        const sinTheta = Math.sin(theta);
+        const x = radius * sinTheta * Math.cos(phi);
+        const y = radius * sinTheta * Math.sin(phi);
+        const z = radius * Math.cos(theta);
+    
+        return new THREE.Vector3(x, y, z);
+    }
+    // Create starfield
+    const starGeometry = new THREE.BufferGeometry();
+    const starMaterial = new THREE.PointsMaterial({ color: 0xffffff });
+    const starVertices = [];
+    for (let i = 0; i < 10000; i++) {
+        // x should be outside the solar system
+        // ranges from [-ChunkSize, -2*ChunkSize] and [*ChunkSize, 2*ChunkSize]
+        const point = getSphericalRandomDot(2 * chunkSize);
+        starVertices.push(point.x, point.y, point.z);
+    }
+    starGeometry.setAttribute('position', new THREE.Float32BufferAttribute(starVertices, 3));
+    const stars = new THREE.Points(starGeometry, starMaterial);
+    scene.add(stars);
+
+    // Add motion blur effect
+    const composer = new EffectComposer(renderer);
+    const renderPass = new RenderPass(scene, camera);
+    composer.addPass(renderPass);
+
+    const afterimagePass = new AfterimagePass(0.5);
+    composer.addPass(afterimagePass);
 
     Promise.all([
         PhysicsInstance.init(),
     ]).then(() => {
 
-        const spaceCraftPosition = new THREE.Vector3(0, 0, 50);
+        const spaceCraftPosition = new THREE.Vector3(0, 0, chunkSize / 2);
+        camera.position.set(0, 0, chunkSize);
         const spacecraft = new Spacecraft(spaceCraftPosition , scene,  () => {
             camera.setFollowTarget(spacecraft.getMesh());
         });
@@ -49,10 +104,10 @@ export function initScene(container){
 
         function animate() {
             requestAnimationFrame(animate);
-            const deltaTime = 1.0/60.0;
+            const deltaTime = 1.0/60.0 * speedMultiplier;
 
             celestialBodies.forEach(body => {
-                body.rotate();
+                body.rotate(deltaTime);
                 if (body instanceof Planet || body instanceof Moon) {
                     body.updateOrbit(deltaTime);
                 }
@@ -60,6 +115,7 @@ export function initScene(container){
             
             detectRaycast();
 
+            // composer.render();
             PhysicsInstance.update(deltaTime);
             spacecraft.update(deltaTime);
             camera.update();
